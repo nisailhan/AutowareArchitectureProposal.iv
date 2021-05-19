7fa693a7399df3f17ac27ec7436066eb356a7814
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-05-19 22:39:29 +0900

    Fix topic name of autoware_perception_rviz_plugin (#1277)
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- common/util/rviz_plugins/autoware_perception_rviz_plugin/src/tools/car_pose.cpp
+++ common/util/rviz_plugins/autoware_perception_rviz_plugin/src/tools/car_pose.cpp
@@ -46,7 +46,7 @@ CarInitialPoseTool::CarInitialPoseTool()
   shortcut_key_ = 'k';
 
   topic_property_ = new StringProperty(
-    "Pose Topic", "/simulation/dummy_perception/publisher/object_info",
+    "Pose Topic", "/simulation/dummy_perception_publisher/object_info",
     "The topic on which to publish dummy object info.", getPropertyContainer(), SLOT(updateTopic()),
     this);
   std_dev_x_ = new FloatProperty(
--- common/util/rviz_plugins/autoware_perception_rviz_plugin/src/tools/pedestrian_pose.cpp
+++ common/util/rviz_plugins/autoware_perception_rviz_plugin/src/tools/pedestrian_pose.cpp
@@ -46,7 +46,7 @@ PedestrianInitialPoseTool::PedestrianInitialPoseTool()
   shortcut_key_ = 'l';
 
   topic_property_ = new StringProperty(
-    "Pose Topic", "/simulation/dummy_perception/publisher/object_info",
+    "Pose Topic", "/simulation/dummy_perception_publisher/object_info",
     "The topic on which to publish dummy object info.", getPropertyContainer(), SLOT(updateTopic()),
     this);
   std_dev_x_ = new FloatProperty(
