3a5bd729c6c5dc34322518598814b554b5307350
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-05-19 22:12:12 +0900

    Feature/refactor stop line module (#1343)
    * Refactor stop_line planner module
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add document
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix English
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/include/scene_module/stop_line/scene.h
+++ planning/scenario_planning/lane_driving/behavior_planning/behavior_velocity_planner/include/scene_module/stop_line/scene.h
@@ -17,6 +17,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include <boost/assert.hpp>
 #include <boost/assign/list_of.hpp>
@@ -32,17 +33,35 @@
 #include <lanelet2_routing/RoutingGraph.h>
 
 #include <scene_module/scene_module_interface.h>
+#include <utilization/boost_geometry_helper.h>
 
 class StopLineModule : public SceneModuleInterface
 {
 public:
-  enum class State { APPROACH, STOP, START };
+  enum class State { APPROACH, STOPPED, START };
+
+  struct SegmentIndexWithPose
+  {
+    size_t index;
+    geometry_msgs::Pose pose;
+  };
+
+  struct SegmentIndexWithPoint2d
+  {
+    size_t index;
+    Point2d point;
+  };
+
+  struct SegmentIndexWithOffset
+  {
+    size_t index;
+    double offset;
+  };
 
   struct DebugData
   {
     double base_link2front;
-    std::vector<geometry_msgs::Pose> stop_poses;
-    geometry_msgs::Pose first_stop_pose;
+    boost::optional<geometry_msgs::Pose> stop_pose;
   };
 
   struct PlannerParam
@@ -65,12 +84,23 @@ public:
 private:
   int64_t module_id_;
 
-  bool getBackwardPointFromBasePoint(
-    const Eigen::Vector2d & line_point1, const Eigen::Vector2d & line_point2,
-    const Eigen::Vector2d & base_point, const double backward_length,
-    Eigen::Vector2d & output_point);
-
   geometry_msgs::Point getCenterOfStopLine(const lanelet::ConstLineString3d & stop_line);
+
+  boost::optional<StopLineModule::SegmentIndexWithPoint2d> findCollision(
+    const autoware_planning_msgs::PathWithLaneId & path, const LineString2d & stop_line);
+
+  boost::optional<StopLineModule::SegmentIndexWithOffset> findOffsetSegment(
+    const autoware_planning_msgs::PathWithLaneId & path,
+    const StopLineModule::SegmentIndexWithPoint2d & collision);
+
+  boost::optional<StopLineModule::SegmentIndexWithPose> calcStopPose(
+    const autoware_planning_msgs::PathWithLaneId & path,
+    const boost::optional<StopLineModule::SegmentIndexWithOffset> & offset_segment);
+
+  autoware_planning_msgs::PathWithLaneId insertStopPose(
+    const autoware_planning_msgs::PathWithLaneId & path,
+    const StopLineModule::SegmentIndexWithPose & insert_index_with_pose,
+    autoware_planning_msgs::StopReason * stop_reason);
 
   lanelet::ConstLineString3d stop_line_;
   State state_;
