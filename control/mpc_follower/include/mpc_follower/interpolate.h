28d9749d34e0e88bf8b8d0af55cf39600ec4e9ac
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-05-29 23:21:33 +0900

    Remove spline interpolate in mpc follower (#1394)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- control/mpc_follower/include/mpc_follower/interpolate.h
+++ control/mpc_follower/include/mpc_follower/interpolate.h
@@ -31,23 +31,3 @@ public:
     const std::vector<double> & base_index, const std::vector<double> & base_value,
     const double & return_index, double & return_value);
 };
-
-class SplineInterpolate
-{
-  bool initialized_;
-  std::vector<double> a_;  //!< @brief temporal vector for calculation
-  std::vector<double> b_;  //!< @brief temporal vector for calculation
-  std::vector<double> c_;  //!< @brief temporal vector for calculation
-  std::vector<double> d_;  //!< @brief temporal vector for calculation
-
-public:
-  SplineInterpolate();
-  SplineInterpolate(const std::vector<double> & x);
-  ~SplineInterpolate();
-  void generateSpline(const std::vector<double> & x);
-  double getValue(const double & s);
-  bool interpolate(
-    const std::vector<double> & base_index, const std::vector<double> & base_value,
-    const std::vector<double> & return_index, std::vector<double> & return_value);
-  void getValueVector(const std::vector<double> & s_v, std::vector<double> & value_v);
-};
